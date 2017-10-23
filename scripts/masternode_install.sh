1b42c1a6dca97288fad61f586a60be10fe06fd0c
Florian Maier <florian.maier@maibornwolff.de>
2017-10-23 20:30:19 +0200

    first changes for templating of config files

--- scripts/masternode_install.sh
+++ scripts/masternode_install.sh
@@ -106,16 +106,10 @@ function prepare_mn_interfaces() {
     
 	# create the additional ipv6 interfaces, rc.local because it's more generic 
 	for NUM in $(seq 1 ${SETUP_MNODES_COUNT}); do
-	
+
 	    # check if the interfaces exist	    
 	    ip -6 addr | grep -qi "${IPV6_INT_BASE}:${NETWORK_BASE_TAG}::${NUM}" && echo "in or out?"
-
-        # echo "didn't find interface!"
-        # echo "ip -6 addr | grep '${IPV6_INT_BASE}:${NETWORK_BASE_TAG}::${NUM}'"
-	    # echo "ip -6 addr add ${IPV6_INT_BASE}:${NETWORK_BASE_TAG}::${NUM}/64 dev ${ETH_INTERFACE}" >> ${NETWORK_CONFIG}
-	    # sleep 2
-	    # ip -6 addr add ${IPV6_INT_BASE}:${NETWORK_BASE_TAG}::${NUM}/64 dev ${ETH_INTERFACE} 	
-
+		
 	done
 }
 
