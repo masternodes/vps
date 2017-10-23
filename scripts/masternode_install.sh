1b8dc9e05cb22fe52aad00e8855a4900f906eff7
Florian Maier <florian.maier@maibornwolff.de>
2017-10-23 19:56:59 +0200

    first changes for templating of config files

--- scripts/masternode_install.sh
+++ scripts/masternode_install.sh
@@ -106,11 +106,15 @@ function prepare_mn_interfaces() {
     
 	# create the additional ipv6 interfaces, rc.local because it's more generic 
 	for NUM in $(seq 1 ${SETUP_MNODES_COUNT}); do
-		echo "ip -6 addr add ${IPV6_INT_BASE}:${NETWORK_BASE_TAG}::${NUM}/64 dev ${ETH_INTERFACE}" >> ${NETWORK_CONFIG}
-		sleep 2
-		# also run it directly to avoid a reboot now
-		#ip -6 addr add ${IPV6_INT_BASE}:${NETWORK_BASE_TAG}::${NUM}/64 dev ${ETH_INTERFACE}
-		ip -6 addr add ${IPV6_INT_BASE}:${NETWORK_BASE_TAG}::${NUM}/64 dev ${ETH_INTERFACE}
+	     # check if the interfaces exist
+	     ip -6 addr | grep '${IPV6_INT_BASE}:${NETWORK_BASE_TAG}::${NUM}/64' &> /dev/null
+	     if [ $? == 0 ]; then
+	         echo "IP already configured, skipping ..."
+	     else
+	         echo "ip -6 addr add ${IPV6_INT_BASE}:${NETWORK_BASE_TAG}::${NUM}/64 dev ${ETH_INTERFACE}" >> ${NETWORK_CONFIG}
+	         sleep 2
+	         ip -6 addr add ${IPV6_INT_BASE}:${NETWORK_BASE_TAG}::${NUM}/64 dev ${ETH_INTERFACE}
+	     fi		
 	done
 }
 
