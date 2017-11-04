45b0f006a32a6bf7521c03fcb8f4e99246059934
Florian Maier <florian.maier@maibornwolff.de>
2017-11-04 16:35:24 +0100

    refactoring

--- install.sh
+++ install.sh
@@ -146,23 +146,28 @@ fi
 # “b” and “argb” have no arguments, acting as sort of a flag.
 # “c” and “argc” have required arguments.
 
+
+# NEW:
+# “n” and “number” have an optional argument with a default value of "1".
+
+
 # set an initial value for the flag
 ARG_B=0
 
 # read the options
-TEMP=`getopt -o a::bc: --long arga::,argb,argc: -n 'test.sh' -- "$@"`
+TEMP=`getopt -o n::bc: --long number::,argb,argc: -n 'test.sh' -- "$@"`
 eval set -- "$TEMP"
 
 # extract options and their arguments into variables.
 while true ; do
     case "$1" in
-        -a|--arga)
+        -n|--number)
             case "$2" in
-                "") ARG_A='some default value' ; shift 2 ;;
+                "") ARG_A='1' ; shift 2 ;;
                 *) ARG_A=$2 ; shift 2 ;;
             esac ;;
         -b|--argb) ARG_B=1 ; shift ;;
-        -c|--argc)
+        -n|--argc)
             case "$2" in
                 "") shift 2 ;;
                 *) ARG_C=$2
