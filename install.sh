d58857df620f70bb4ad5a0523cb9bb40be64c6d1
Florian Maier <florian.maier@maibornwolff.de>
2017-11-04 18:01:02 +0100

    refactoring

--- install.sh
+++ install.sh
@@ -215,7 +215,7 @@ do
 done
 
 
-[ -n "${count}" ] && count="${SETUP_MNODES_COUNT}"
+# [ -n "${count}" ] && count="${SETUP_MNODES_COUNT}"
 # [ -n "${ipaddr_list}" ] && DEFAULT_IPADDR="${ipaddr_list}"
 # [ -n "${user}" ] && DEFAULT_USER="${user}"
 # [ -n "${passwd}" ] && DEFAULT_PAWD="${passwd}"
