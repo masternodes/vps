e5f542208bd4f2de6243d4beb95efd5b367bd11a
Florian Maier <florian.maier@maibornwolff.de>
2017-11-20 20:26:16 +0100

    add initial sentinel work

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@
 #  ╚═╝  ╚═══╝ ╚═════╝ ╚═════╝ ╚══════╝╚═╝     ╚═╝╚═╝  ╚═╝╚══════╝   ╚═╝   ╚══════╝╚═╝  ╚═╝
 #                                                              ╚╗ @marsmensch 2016-2017 ╔╝                   				
 #                   
-# version 	0.7.6
+# version 	0.7.7
 # date    	2017-11-16
 #
 # function:	part of the masternode scripts, source the proper config file
@@ -30,7 +30,7 @@ declare -r CRYPTOS=`ls -l config/ | egrep '^d' | awk '{print $9}' | xargs echo -
 declare -r DATE_STAMP="$(date +%y-%m-%d-%s)"
 declare -r SCRIPTPATH=$( cd $(dirname ${BASH_SOURCE[0]}) > /dev/null; pwd -P )
 declare -r MASTERPATH="$(dirname "${SCRIPTPATH}")"
-declare -r SCRIPT_VERSION="v0.7.6"
+declare -r SCRIPT_VERSION="v0.7.7"
 declare -r SCRIPT_LOGFILE="/tmp/nodemaster_${DATE_STAMP}_out.log"
 declare -r IPV4_DOC_LINK="https://www.vultr.com/docs/add-secondary-ipv4-address"
 
@@ -79,6 +79,7 @@ function show_help(){
     echo "-n or --net: IP address type t be used (4 vs. 6).";
     echo "-c or --count: Number of masternodes to be installed.";
     echo "-r or --release: Release version to be installed.";
+    echo "-s or --sentinel: Add sentinel monitoring for a node type. Combine with the -p option";    
     echo "-w or --wipe: Wipe ALL local data for a node type. Combine with the -p option";
     echo "-u or --update: Update a specific masternode daemon. Combine with the -p option";
     exit 1;
@@ -115,7 +116,7 @@ function install_packages() {
     automake libcurl4-openssl-dev libboost-all-dev libssl-dev libdb++-dev \
     make autoconf automake libtool git apt-utils libprotobuf-dev pkg-config \
     libcurl3-dev libudev-dev libqrencode-dev bsdmainutils pkg-config libssl-dev \
-    libgmp3-dev libevent-dev jp2a pv 	&>> ${SCRIPT_LOGFILE}
+    libgmp3-dev libevent-dev jp2a pv virtualenv	&>> ${SCRIPT_LOGFILE}
 }
 
 #
@@ -158,6 +159,82 @@ function create_mn_user() {
 # /* no parameters, creates a masternode data directory (one per masternode)  */
 #
 function create_mn_dirs() {
+
+    # individual data dirs for now to avoid problems
+    echo "* Creating masternode directories"
+    mkdir -p ${MNODE_CONF_BASE}
+	for NUM in $(seq 1 ${count}); do
+	    if [ ! -d "${MNODE_DATA_BASE}/${CODENAME}${NUM}" ]; then
+	         echo "creating data directory ${MNODE_DATA_BASE}/${CODENAME}${NUM}" &>> ${SCRIPT_LOGFILE}
+             mkdir -p ${MNODE_DATA_BASE}/${CODENAME}${NUM} &>> ${SCRIPT_LOGFILE}
+        fi
+	done    
+	
+}
+
+#
+# /* no parameters, creates a sentinel config for a set of masternodes (one per masternode)  */
+#
+function create_sentinel_setup() {
+
+#
+# 1st => Delete and re-index masternode config
+#
+# Before starting the steps make sure to restart your masternode with
+# reindex so you are using the fresh blockchain. Delete mncache.dat and
+# mnpayments.dat from your vivocore folder before reindexing.
+# cd .vivocore   // Your Vivocore folder
+# ./vivo-cli stop
+# rm mncache.dat
+# rm mnpayments.dat
+# ./vivod -daemon -reindex
+#
+# 2nd => Clone and install ONE Sentinel instance
+#
+# git clone https://github.com/dashpay/sentinel.git
+# cd sentinel
+# git clone https://github.com/dashpay/sentinel.git
+# virtualenv venv
+# venv/bin/pip install -r requirements.txt
+# 
+# 3rd => Configure multiple Sentinels for every masternode
+# => sentinel.conf
+# Uncomment the #dash_conf line at the top and add the path to your MN’s vivo.conf. S
+# venv/bin/python bin/sentinel.py
+# 
+# You should see: “vivod not synced with network! Awaiting full sync before running Sentinel.”
+#
+# Wait until the reindex has complete and the wallet has sync’d
+# ./vivo-cli mnsync status
+# This is what you’re waiting to see:
+# 
+# AssetId 999, all trues, one false, and a FINISHED
+# {
+# “AssetID”: 999,
+# “AssetName”: “MASTERNODE_SYNC_FINISHED”,
+# “Attempt”: 0,
+# “IsBlockchainSynced”: true,
+# “IsMasternodeListSynced”: true,
+# “IsWinnersListSynced”: true,
+# “IsSynced”: true,
+# “IsFailed”: false
+# }
+#  
+# 
+# At this point, your remote masternode is synchronized and chatting with the network
+# but is not accepted as a masternode because it hasn’t been introduced to the network
+# by your collateral
+#
+# venv/bin/python bin/sentinel.py
+# should return nothing but silence. This is how you know it’s working, and your masternode is working.
+#
+# 5th => Create a crontab entry as masternode user (!) to wake sentinel every five minutes
+#
+#
+#
+#crontab -e
+#
+#* * * * * cd /home/YOURUSERNAME/.vivocore/sentinel && ./venv/bin/python bin/sentinel.py 2>&1 >> sentinel-cron.log
 
     # individual data dirs for now to avoid problems
     echo "* Creating masternode directories"
@@ -242,8 +319,12 @@ function create_mn_configuration() {
         
 }
 
+#
+# /* no parameters, generates a masternode configuration file per masternode in the default */
+# 
 function create_control_configuration() {
 
+    # delete any old stuff that's still around
     rm -f /tmp/${CODENAME}_masternode.conf &>> ${SCRIPT_LOGFILE}
 	# create one line per masternode with the data we have
 	for NUM in $(seq 1 ${count}); do
@@ -254,6 +335,9 @@ function create_control_configuration() {
 
 }
 
+#
+# /* no parameters, generates a a pre-populated masternode systemd config file */
+# 
 function create_systemd_configuration() {
 
     echo "* (over)writing systemd config files for masternodes"
@@ -290,6 +374,9 @@ function create_systemd_configuration() {
 
 }
 
+#
+# /* set all permissions to the masternode user */
+# 
 function set_permissions() {
 
 	# maybe add a sudoers entry later
@@ -297,6 +384,9 @@ function set_permissions() {
 
 }
 
+#
+# /* wipe all files and folders generated by the script for a specific project */
+# 
 function wipe_all() {
     
     echo "Deleting all ${project} related data!" 
@@ -309,6 +399,12 @@ function wipe_all() {
 
 }
 
+#
+# /*
+# remove packages and stuff we don't need anymore and set some recommended
+# kernel parameters
+# */
+# 
 function cleanup_after() {
 
 	apt-get -qqy -o=Dpkg::Use-Pty=0 --force-yes autoremove
@@ -331,6 +427,10 @@ function cleanup_after() {
 	
 }
 
+#
+# /* project as parameter, sources the project specific parameters and runs the main logic */
+# 
+
 # source the default and desired crypto configuration files
 function source_config() {
 
@@ -340,8 +440,6 @@ function source_config() {
     check_distro
         
 	if [ -f ${SETUP_CONF_FILE} ]; then
-		#echo "read default config"	
-		#source config/default.env
 		echo "Script version ${SCRIPT_VERSION}, you picked: ${project}"
 		echo "apply config file for ${project}"	&>> ${SCRIPT_LOGFILE}	
 		source "${SETUP_CONF_FILE}"
@@ -420,6 +518,9 @@ function source_config() {
 	
 }
 
+#
+# /* no parameters, builds the required masternode binary from sources. Exits if already exists and "update" not given  */
+# 
 function build_mn_from_source() {
         # daemon not found compile it
         if [ ! -f ${MNODE_DAEMON} ]; then
@@ -451,6 +552,9 @@ function build_mn_from_source() {
         fi
 }
 
+#
+# /* no parameters, print some (hopefully) helpful advice  */
+# 
 function final_call() {
 	# note outstanding tasks that need manual work
     echo "************! ALMOST DONE !******************************"	
@@ -474,7 +578,9 @@ function final_call() {
 	tput sgr0
 }
 
-
+#
+# /* no parameters, create the required network configuration. IPv6 is auto.  */
+# 
 function prepare_mn_interfaces() {
     
     IPV6_INT_BASE="$(ip -6 addr show dev ${ETH_INTERFACE} | grep inet6 | awk -F '[ \t]+|/' '{print $3}' | grep -v ^fe80 | grep -v ^::1 | cut -f1-4 -d':' | head -1)"
@@ -606,14 +712,6 @@ if [ "$wipe" -eq 1 ]; then
 	get_confirmation "Would you really like to WIPE ALL DATA!? YES/NO y/n" && wipe_all
 	exit 0
 fi		
- 
-## Iterate over rest arguments called $arg
-# for arg in "$@"
-# do
-#     # Your code here (remove example below)
-#     echo $arg
-#  
-# done
 
 #################################################
 # source default config before everything else
