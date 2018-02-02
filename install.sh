456b53e6d18b6a173b022e39c10c2c1b8b1f3961
Florian Maier <florian.maier@maibornwolff.de>
2018-02-02 13:49:45 +0100

    add DO ipv6 workaround

--- install.sh
+++ install.sh
@@ -594,13 +594,11 @@ function prepare_mn_interfaces() {
         exit 1
     fi
 
-	# DO ipv6 fix, are we on DO?
-	if [ ! -z $(grep --quiet "::8888" ${DO_NET_CONF}) ]; then
-	    echo "FOUND";
-	else
-		sed -i '/iface eth0 inet static/a dns-nameservers 2001:4860:4860::8844 2001:4860:4860::8888 8.8.8.8 127.0.0.1' ${DO_NET_CONF}
-        ifdown ${ETH_INTERFACE}; ifup ${ETH_INTERFACE};	        
-	fi
+    # DO ipv6 fix, are we on DO?
+    if [ -f ${DO_NET_CONF} ]; && [ ! -z $(grep --quiet "::8888" ${DO_NET_CONF}) ]; then
+        sed -i '/iface eth0 inet static/a dns-nameservers 2001:4860:4860::8844 2001:4860:4860::8888 8.8.8.8 127.0.0.1' ${DO_NET_CONF}
+        ifdown ${ETH_INTERFACE}; ifup ${ETH_INTERFACE};	 
+    fi
 
     IPV6_INT_BASE="$(ip -6 addr show dev ${ETH_INTERFACE} | grep inet6 | awk -F '[ \t]+|/' '{print $3}' | grep -v ^fe80 | grep -v ^::1 | cut -f1-4 -d':' | head -1)" &>> ${SCRIPT_LOGFILE}
 	
