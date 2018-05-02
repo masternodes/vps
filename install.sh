857263491cab7fc55256765d7384db2b291de058
Florian Maier <florian.maier@maibornwolff.de>
2018-05-02 22:28:38 +0200

    add bdb4.8 to default setup, always use this from now on

--- install.sh
+++ install.sh
@@ -114,13 +114,14 @@ function install_packages() {
     # development and build packages
     # these are common on all cryptos
     echo "* Package installation!"
+    add-apt-repository -y ppa:bitcoin/bitcoin
     apt-get -qq -o=Dpkg::Use-Pty=0 -o=Acquire::ForceIPv4=true update
     apt-get -qqy -o=Dpkg::Use-Pty=0 -o=Acquire::ForceIPv4=true install build-essential g++ \
     protobuf-compiler libboost-all-dev autotools-dev \
     automake libcurl4-openssl-dev libboost-all-dev libssl-dev libdb++-dev \
     make autoconf automake libtool git apt-utils libprotobuf-dev pkg-config \
     libcurl3-dev libudev-dev libqrencode-dev bsdmainutils pkg-config libssl-dev \
-    libgmp3-dev libevent-dev jp2a pv virtualenv	&>> ${SCRIPT_LOGFILE}
+    libgmp3-dev libevent-dev jp2a pv virtualenv	libdb4.8-dev libdb4.8++-dev &>> ${SCRIPT_LOGFILE}
 }
 
 #
